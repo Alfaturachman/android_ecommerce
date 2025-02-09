46303a97d9ee30921b18e31d7e002f66aa9aad72
alfaturachman <122202202929@mhs.dinus.ac.id>
2025-02-09 20:52:34 +0700

    initial commit

--- app/src/main/java/com/example/lestarithriftshop/ServerAPI.java
+++ app/src/main/java/com/example/lestarithriftshop/ServerAPI.java
@@ -1,8 +1,11 @@
 package com.example.lestarithriftshop;
 
 public class ServerAPI {
-    public static final String BASE_URL = "http://192.168.101.48/android_ecommerce/";
-    public static final String URL_FOTO = "http://192.168.101.48/android_ecommerce/produk/";
-    public static final String BASE_URL_PROFILE = "http://192.168.101.48/android_ecommerce/foto_profile/";
-    public static final String BASE_URL_PEMBAYARAN = "http://192.168.101.48/android_ecommerce/bukti_pembayaran/";
+    private static final String IP_ADDRESS = "192.168.1.39";
+    private static final String BASE_PATH = "http://" + IP_ADDRESS + "/android_ecommerce/";
+
+    public static final String BASE_URL = BASE_PATH;
+    public static final String URL_FOTO = BASE_PATH + "produk/";
+    public static final String BASE_URL_PROFILE = BASE_PATH + "foto_profile/";
+    public static final String BASE_URL_PEMBAYARAN = BASE_PATH + "bukti_pembayaran/";
 }
